18305119281ce720497bf55c11342e34680b85e5
andrea <andrea.orione02@gmail.com>
2022-07-13 10:43:59 +0200

    Added awesome installation and plugins

--- Arch-Installer.sh
+++ Arch-Installer.sh
@@ -7,8 +7,9 @@ USER_HOME=$PWD;
 $(pacman -Sy);
 cd $DOTFILES;
 
+# PACKAGES INSTALLATION {{{
 # should add htop
-REQUIRED=('neofetch' 'vim' 'xorg' 'picom' 'alacritty' 'firefox' 'lightdm' 'lightdm-webkit2-greeter');
+REQUIRED=('neofetch' 'vim' 'xorg' 'picom' 'alacritty' 'firefox' 'lightdm' 'lightdm-webkit2-greeter', 'base-devel');
 ABSENT=();
 
 echo "Checking installed packages";
@@ -18,21 +19,59 @@ for i in ${REQUIRED[@]}; do
 	fi
 done
 
-echo "Installation"
+echo "Installation";
 for i in ${ABSENT[@]}; do
 	echo "Installing $i";
 	$(pacman -S $i);
 done
 
-echo "Setting up lightdm"
-$(systemctl enable lightdm)
-cp lightdm.conf /etc/lightdm
-echo "Coping config file"
-echo "Coping .vimrc"
-cp .vimrc $USER_HOME
-echo "Installing vim plugins"
-$(curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim)
-echo "Coping neofetch config"
-mkdir -p $USER_HOME/.config/neofetch
-cp .config/neofetch/config.conf $USER_HOME/.config/neofetch
+echo "Installing yay";
+cd $USER_HOME;
+$(git clone https://aur.archlinux.org/yay-git.git);
+cd yay-git;
+$(makepkg -si);
+cd $USER_HOME;
+rm -r yay-git;
+cd $DOTFILES;
 
+$(yay -Sy);
+AUR_REQUIRED=('awesome-freedesktop-git', 'lain-git');
+AUR_ABSENT=();
+for i in ${AUR_REQUIRED[@]}; do
+	if ! yay -Qs $i > /dev/null; then
+		AUR_ABSENT+=($i);
+	fi
+done
+
+for i in ${AUR_ABSENT[@]}; do
+	echo "Installing $i";
+	$(yay -S $i);
+done
+
+# }}}
+
+
+# CONFIG FILE PLACING {{{
+# lightdm
+echo "Setting up lightdm";
+$(systemctl enable lightdm);
+cp lightdm.conf /etc/lightdm;
+echo "Coping config file";
+
+# vim
+echo "Coping .vimrc";
+cp .vimrc $USER_HOME;
+echo "Installing vim plugins";
+$(curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim);
+
+# neofetch
+echo "Coping neofetch config";
+mkdir -p $USER_HOME/.config/neofetch;
+cp .config/neofetch/config.conf $USER_HOME/.config/neofetch;
+
+# awsome
+echo "Coping awesome config files";
+cp -r .config/awesome $USER_HOME/.config;
+
+
+# }}}
