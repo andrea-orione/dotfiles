a937fa08651481c52f60f23c43c4142aa49c1fc8
andrea-orione <andrea.orione02@gmail.com>
2022-07-15 07:02:43 +0200

    Stuff

--- .config/awesome/themes/orion-blue/theme.lua
+++ .config/awesome/themes/orion-blue/theme.lua
@@ -21,12 +21,21 @@ local my_table = awful.util.table or gears.table --Binding tale 4.{0,1} compatib
 
 -- VARIABLE DEFINITION {{{
 local theme                                     = {}
+-- Folders
 theme.confdir                                   = os.getenv("HOME") .. "/.config/awesome/themes/orion-blue"
 theme.wallpapers_folder                         = os.getenv("HOME") .. "/.wallpapers"
+-- Fonts
 theme.font                                      = "Ubuntu Regular 11"
 theme.taglist_font                              = "Ubuntu Regular 11"
+-- Menu
 theme.menu_bg_normal                            = "#000000"
 theme.menu_bg_focus                             = "#000000"
+theme.menu_border_width                         = 2
+theme.menu_width                                = 140
+theme.menu_submenu_icon                         = theme.confdir .. "/icons/submenu.png"
+theme.menu_fg_normal                            = "#aaaaaa"
+theme.menu_fg_focus                             = "#ff8c00"
+-- Bars
 theme.bg_normal                                 = "#000000"
 theme.bg_focus                                  = "#000000"
 theme.bg_urgent                                 = "#000000"
@@ -34,15 +43,13 @@ theme.fg_normal                                 = "#aaaaaa"
 theme.fg_focus                                  = "#ff8c00"
 theme.fg_urgent                                 = "#af1d18"
 theme.fg_minimize                               = "#ffffff"
+-- Clients
 theme.border_width                              = 2
 theme.border_normal                             = "#1c2022"
 theme.border_focus                              = "#606060"
 theme.border_marked                             = "#3ca4d8"
-theme.menu_border_width                         = 2
-theme.menu_width                                = 140
-theme.menu_submenu_icon                         = theme.confdir .. "/icons/submenu.png"
-theme.menu_fg_normal                            = "#aaaaaa"
-theme.menu_fg_focus                             = "#ff8c00"
+theme.useless_gap                               = 4
+-- Icons
 theme.widget_temp                               = theme.confdir .. "/icons/thermometer.svg"
 theme.widget_uptime                             = theme.confdir .. "/icons/ac.png"
 theme.widget_cpu                                = theme.confdir .. "/icons/cpu.svg"
@@ -61,22 +68,19 @@ theme.widget_music_pause                        = theme.confdir .. "/icons/pause
 theme.widget_music_stop                         = theme.confdir .. "/icons/stop.png"
 theme.taglist_squares_sel                       = theme.confdir .. "/icons/square_a.png"
 theme.taglist_squares_unsel                     = theme.confdir .. "/icons/square_b.png"
-theme.tasklist_plain_task_name                  = true
-theme.tasklist_disable_icon                     = true
-theme.useless_gap                               = 4
-theme.layout_tile                               = theme.confdir .. "/icons/tile.png"
-theme.layout_tilegaps                           = theme.confdir .. "/icons/tilegaps.png"
-theme.layout_tileleft                           = theme.confdir .. "/icons/tileleft.png"
-theme.layout_tilebottom                         = theme.confdir .. "/icons/tilebottom.png"
-theme.layout_tiletop                            = theme.confdir .. "/icons/tiletop.png"
-theme.layout_fairv                              = theme.confdir .. "/icons/fairv.png"
-theme.layout_fairh                              = theme.confdir .. "/icons/fairh.png"
-theme.layout_spiral                             = theme.confdir .. "/icons/spiral.png"
-theme.layout_dwindle                            = theme.confdir .. "/icons/dwindle.png"
-theme.layout_max                                = theme.confdir .. "/icons/max.png"
-theme.layout_fullscreen                         = theme.confdir .. "/icons/fullscreen.png"
-theme.layout_magnifier                          = theme.confdir .. "/icons/magnifier.png"
-theme.layout_floating                           = theme.confdir .. "/icons/floating.png"
+--theme.layout_tile                               = theme.confdir .. "/icons/tile.png"
+--theme.layout_tilegaps                           = theme.confdir .. "/icons/tilegaps.png"
+--theme.layout_tileleft                           = theme.confdir .. "/icons/tileleft.png"
+--theme.layout_tilebottom                         = theme.confdir .. "/icons/tilebottom.png"
+--theme.layout_tiletop                            = theme.confdir .. "/icons/tiletop.png"
+--theme.layout_fairv                              = theme.confdir .. "/icons/fairv.png"
+--theme.layout_fairh                              = theme.confdir .. "/icons/fairh.png"
+--theme.layout_spiral                             = theme.confdir .. "/icons/spiral.png"
+--theme.layout_dwindle                            = theme.confdir .. "/icons/dwindle.png"
+--theme.layout_max                                = theme.confdir .. "/icons/max.png"
+--theme.layout_fullscreen                         = theme.confdir .. "/icons/fullscreen.png"
+--theme.layout_magnifier                          = theme.confdir .. "/icons/magnifier.png"
+--theme.layout_floating                           = theme.confdir .. "/icons/floating.png"
 theme.titlebar_close_button_normal              = theme.confdir .. "/icons/titlebar/close_normal.png"
 theme.titlebar_close_button_focus               = theme.confdir .. "/icons/titlebar/close_focus.png"
 theme.titlebar_minimize_button_normal           = theme.confdir .. "/icons/titlebar/minimize_normal.png"
