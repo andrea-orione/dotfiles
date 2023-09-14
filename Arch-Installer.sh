cf68def70a8bfa9720a943dbc8633b089d0e714a
Andrea <andrea.orione02@gmail.com>
2023-09-14 15:22:30 +0200

    Forgotten stuff

--- Arch-Installer.sh
+++ Arch-Installer.sh
@@ -9,7 +9,7 @@ cd $DOTFILES;
 
 # PACKAGES INSTALLATION {{{
 # should add htop
-REQUIRED=('neofetch' 'vim' 'neovim' 'firefox' 'sddm' 'base-devel' 'ttf-ubuntu-font-family' 'vlc' 'rhytmbox' 'okular' 'emacs' 'alacritty' 'pipewire' 'alsa-firmware' 'alsa-plugins' 'alsa-utils' 'htop' 'eza' 'firefox' 'code' 'code-features');
+REQUIRED=('neofetch' 'vim' 'neovim' 'firefox' 'sddm' 'base-devel' 'ttf-ubuntu-font-family' 'vlc' 'rhytmbox' 'okular' 'emacs' 'alacritty' 'pipewire' 'alsa-firmware' 'alsa-plugins' 'alsa-utils' 'htop' 'eza' 'firefox' 'code' 'code-features' 'dunst' 'waybar');
 
 echo "Installation";
 for i in ${REQUIRED[@]}; do
@@ -48,17 +48,35 @@ echo "Coping config file";
 
 # vim
 echo "Coping .vimrc";
-cp .vimrc $USER_HOME;
+cp .vimrc $USER_HOME/;
 echo "Installing vim plugins";
 $(curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim);
 
 # neofetch
 echo "Coping neofetch config";
-cp -r .config/neofetch $USER_HOME/.config;
+cp -r .config/neofetch $USER_HOME/.config/;
 
 # awsome
 echo "Coping awesome config files";
-cp -r .config/awesome $USER_HOME/.config;
+cp -r .config/awesome $USER_HOME/.config/;
+
+echo "Coping alacritty config";
+cp -r .config/alacritty $USER_HOME/.config/;
+
+echo "Coping dunst config";
+cp -r .config/dunst $USER_HOME/.config/;
+
+echo "Coping emacs config";
+cp -r .config/emacs $USER_HOME/.config/;
+
+echo "Coping htop config";
+cp -r .config/htop $USER_HOME/.config/;
+
+echo "Coping hyprland config";
+cp -r .config/hypr $USER_HOME/.config/;
+
+echo "Coping wallpapers";
+cp -r .wallpapers $USER_HOME/;
 
 
 # }}}
